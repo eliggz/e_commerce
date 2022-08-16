e35f940413075f07fab5e9df2292546383887fe5
eliggz <106848206+eliggz@users.noreply.github.com>
2022-08-16 11:40:08 -0300

    Update login.js

---
+++ js/login.js
@@ -0,0 +1,17 @@
+function manejarSubmit(event){
+    event.preventDefault();
+    const contrasena=document.getElementById("contraseña").value;
+    const correo=document.getElementById("correo").value;
+    if (contrasena != "" && correo != "") {
+        window.open("/home.html","_self");
+    }
+    else if (contrasena != "") {
+        alert("Se necesitan llenar los siguientes campos:\n e-mail");
+    }
+    else if (correo != ""){
+        alert("Se necesitan llenar los siguientes campos:\n contraseña");
+    }
+    else {
+        alert("Se necesitan llenar los siguientes campos:\n e-mail \ncontraseña")
+    }
+}
